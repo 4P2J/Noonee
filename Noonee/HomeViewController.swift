ef3a3db407dbf9bbec0b0ca8812353bac241228e
Jae-eun <je6752@gmail.com>
2021-05-22 18:09:21 +0900

    홈화면 버튼 UI 추가

---
+++ Noonee/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Noonee
+//
+//  Created by 이재은 on 2021/05/22.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    @IBOutlet private weak var journeyButton: UIButton!
+    @IBOutlet private weak var historyButton: UIButton!
+    @IBOutlet private weak var settingButton: UIButton!
+
+    private enum ViewMetrics {
+        static let buttonCornerRadius: CGFloat = 20
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setLayout()
+    }
+
+    private func setLayout() {
+        journeyButton.layer.cornerRadius = ViewMetrics.buttonCornerRadius
+        historyButton.layer.cornerRadius = ViewMetrics.buttonCornerRadius
+        settingButton.layer.cornerRadius = ViewMetrics.buttonCornerRadius
+    }
+}
