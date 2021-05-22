234a87e8c310668218905cd1ac1c65936a993e6d
조상호 <97chos@naver.com>
2021-05-22 19:45:55 +0900

    merge to main

---
+++ Noonee/APIService/APIURLs/APIURLs.swift
@@ -0,0 +1,13 @@
+//
+//  APIURLs.swift
+//  Noonee
+//
+//  Created by sangho Cho on 2021/05/22.
+//
+
+import Foundation
+
+enum APIURL: String {
+  case searchPlaceUrl = "https://map.naver.com/v5/api/instantSearch"
+  case searchPathUrl = "https://map.naver.com/v5/api/dir/findpt"
+}
