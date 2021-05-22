a68a1c9a89a301e99ead0d0c16edabd330f6042b
조상호 <97chos@naver.com>
2021-05-22 19:18:23 +0900

    장소 검색 API 추가

--- Noonee/APIService/SearchPlaceAPI/SearchPlaceAPI.swift
+++ Noonee/APIService/SearchPlaceAPI/SearchPlaceAPI.swift
@@ -9,7 +9,36 @@ import Foundation
 
 class SearchPlaceAPI {
   
-  func searchPlace() {
+  func searchPlace(keyword: String, completion: @escaping (Result<Places,(Error)>) -> ()) {
 
+    var urlComponent = URLComponents(string: APIURL.searchPlaceUrl.rawValue)
+
+    let languageQuery = URLQueryItem(name: "lang", value: "ko")
+    let callerQuery = URLQueryItem(name: "caller", value: "pcweb")
+    let typesQuery = URLQueryItem(name: "types", value: "place,address,bus")
+    let coordsQuery = URLQueryItem(name: "coords", value: "37.55019151418508,126.96002483367921")
+    let nameQuery = URLQueryItem(name: "query", value: "\(keyword.utf8)")
+
+    urlComponent?.queryItems = [languageQuery,callerQuery,typesQuery,coordsQuery,nameQuery]
+
+    guard let requestURL = urlComponent?.url else { return }
+
+    URLSession(configuration: .default).dataTask(with: requestURL) { data, response, error in
+      guard error == nil else { return }
+
+      let successRange = 200..<300
+      guard let statusCode = (response as? HTTPURLResponse)?.statusCode, successRange.contains(statusCode) else { return }
+
+      guard let resultData = data else { return }
+
+      do {
+        let places = try JSONDecoder().decode(Places.self, from: resultData)
+        completion(.success(places))
+      } catch {
+        print(error.localizedDescription)
+        completion(.failure(error))
+      }
+    }.resume()
   }
 }
+
