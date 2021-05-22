edabb07ddefcd86521aa657929b48308aea125a5
조상호 <97chos@naver.com>
2021-05-22 19:17:28 +0900

    Paths 모델 생성

---
+++ Noonee/Model/Paths.swift
@@ -0,0 +1,46 @@
+//
+//  paths.swift
+//  Noonee
+//
+//  Created by sangho Cho on 2021/05/22.
+//
+
+import Foundation
+
+struct Paths: Codable {
+  var pathList: [Path]
+
+  struct Path: Codable {
+    var type: String
+    var duration: Int
+    var fare: Int
+    var transferCount: Int
+    var legs: [Steps]
+
+    struct Steps: Codable {
+      var stepList: [Step]
+
+      struct Step: Codable {
+        var type: String
+        var duration: Int
+        var routes: [Route]
+        var stations: [Station]
+
+        struct Route: Codable {
+          var name: String
+        }
+
+        struct Station: Codable {
+          var name: String
+          var displayCode: String
+        }
+      }
+
+      enum CodingKeys: String, CodingKey {
+        case stepList = "steps"
+      }
+    }
+  }
+
+}
+
