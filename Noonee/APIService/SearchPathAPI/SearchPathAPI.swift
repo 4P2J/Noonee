a2f666cc33a7fcf00bddea32274efb4ba5eeb984
조상호 <97chos@naver.com>
2021-05-22 15:37:14 +0900

    APIService 생성

---
+++ Noonee/APIService/SearchPathAPI/SearchPathAPI.swift
@@ -0,0 +1,15 @@
+//
+//  SearchPathAPI.swift
+//  Noonee
+//
+//  Created by sangho Cho on 2021/05/22.
+//
+
+import Foundation
+
+class SearchPathAPI {
+
+  func searchPath() {
+    
+  }
+}
---
+++ Noonee/APIService/SearchPlaceAPI/SearchPlaceAPI.swift
@@ -0,0 +1,15 @@
+//
+//  SearchPlaceAPI.swift
+//  Noonee
+//
+//  Created by sangho Cho on 2021/05/22.
+//
+
+import Foundation
+
+class SearchPlaceAPI {
+  
+  func searchPlace() {
+
+  }
+}
