86994adc80aab4d83f3b1f929af19939062477d1
조상호 <97chos@naver.com>
2021-05-26 23:18:28 +0900

    코드 베이스 기반으로 작성된 SelectViewController에서 StoryBoard 기반으로 변경

--- Noonee/Select/SelectViewController.swift
+++ Noonee/Select/SelectViewController.swift
@@ -9,136 +9,79 @@ import Foundation
 import UIKit
 import SnapKit
 
-final class SelectViewContrller: UIViewController {
+final class SelectViewController: UIViewController {
 
-  // MARK: UI
+    // MARK: UI
 
-  private let recommandButton: UIButton = {
-    let button = UIButton()
-    button.setTitle("Recommend", for: .normal)
-    button.titleLabel?.font = .boldSystemFont(ofSize: 40)
-    button.setTitleColor(.white, for: .normal)
-    button.backgroundColor = UIColor(named: "HomeButtonGray")
-    return button
-  }()
-  private let timeLeastButton: UIButton = {
-    let button = UIButton()
-    button.setTitle("Minimum Time", for: .normal)
-    button.setTitleColor(.white, for: .normal)
-    button.titleLabel?.font = .boldSystemFont(ofSize: 40)
-    button.backgroundColor = UIColor(named: "HomeButtonGray")
-
-    return button
-  }()
-  private let trandsferLeastButton: UIButton = {
-    let button = UIButton()
-    button.setTitle("Miminum Transfer", for: .normal)
-    button.setTitleColor(.white, for: .normal)
-    button.titleLabel?.font = .boldSystemFont(ofSize: 40)
-    button.backgroundColor = UIColor(named: "HomeButtonGray")
-    return button
-  }()
+    @IBOutlet weak var RecommandButton: SelectRouteButton!
+    @IBOutlet weak var TimeLeastButton: SelectRouteButton!
+    @IBOutlet weak var TransferLeastButton: SelectRouteButton!
 
 
-  // MARK: Properties
+    // MARK: Actions
 
-  var depaturePlace: Place
-  var destinationPlace: Place
-  var isDeparture: Bool = true
-  var singletonData: SingletonUserData = SingletonUserData.shared
-  let searchPathAPI = SearchPathAPI()
+    @IBAction func RecommandAction(_ sender: Any) {
+        if let vc = UIStoryboard(name: "Detail", bundle: .main)
+            .instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController {
+            if let depaturePlace = self.singletonData.depaturePlace, let goalPlace = self.singletonData.destinationPlace {
+                self.searchPathAPI.searchPath(startPlace: depaturePlace, goalPlace: goalPlace) { [weak self] in
 
+                    vc.StepList = try! $0.get().pathList[0].legs[0].stepList
+                    vc.totalFare = try! $0.get().pathList[0].fare
+                    DispatchQueue.main.async {
+                        self?.navigationController?.pushViewController(vc, animated: true)
 
-  // MARK: Initialzing
-
-  init(depature: Place, destination: Place) {
-    self.depaturePlace = depature
-    self.destinationPlace = destination
-    super.init(nibName: nil, bundle: nil)
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-
-  // MARK: View LifeCycle
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.configuration()
-    self.layout()
-  }
-
-  override func viewDidLayoutSubviews() {
-    self.recommandButton.layer.cornerRadius = 20
-    self.timeLeastButton.layer.cornerRadius = 20
-    self.trandsferLeastButton.layer.cornerRadius = 20
-  }
-
-  private func configuration() {
-    self.configureView()
-    self.configureNavigation()
-    self.configureButtons()
-  }
-
-  private func configureView() {
-    self.view.backgroundColor = UIColor(named: "backgroundBlack")
-  }
-
-  private func configureNavigation() {
-    self.navigationItem.largeTitleDisplayMode = .always
-    self.navigationItem.title = "Select Route"
-    navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
-  }
-
-  private func configureButtons() {
-    self.recommandButton.addTarget(self, action: #selector(self.recommandButtonSet), for: .touchUpInside)
-    self.timeLeastButton.addTarget(self, action: #selector(self.recommandButtonSet), for: .touchUpInside)
-    self.trandsferLeastButton.addTarget(self, action: #selector(self.recommandButtonSet), for: .touchUpInside)
-  }
-
-
-  // MARK: Button Set
-
-  @objc private func recommandButtonSet() {
-    if let vc = UIStoryboard(name: "Detail", bundle: .main)
-        .instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController {
-      if let depaturePlace = self.singletonData.depaturePlace, let goalPlace = self.singletonData.destinationPlace {
-        self.searchPathAPI.searchPath(startPlace: depaturePlace, goalPlace: goalPlace) { [weak self] in
-          
-          vc.StepList = try! $0.get().pathList[0].legs[0].stepList
-          vc.totalFare = try! $0.get().pathList[0].fare
-          DispatchQueue.main.async {
-            self?.navigationController?.pushViewController(vc, animated: true)
-
-          }
+                    }
+                }
+            }
         }
-      }
     }
-  }
 
-  // MARK: Layout
+    @IBAction func TimeLeastAction(_ sender: Any) {
+    }
 
-  private func layout() {
-    self.view.addSubview(self.recommandButton)
-    self.view.addSubview(self.timeLeastButton)
-    self.view.addSubview(self.trandsferLeastButton)
+    @IBAction func TransferLeastAction(_ sender: Any) {
+    }
 
-    self.recommandButton.snp.makeConstraints {
-      $0.top.equalTo(self.view.safeAreaLayoutGuide).inset(38)
-      $0.leading.trailing.equalToSuperview().inset(16)
-      $0.height.equalTo(130)
+    private enum ViewMetrics {
+      static let buttonCornerRadius: CGFloat = 20
     }
-    self.timeLeastButton.snp.makeConstraints{
-      $0.top.equalTo(self.recommandButton.snp.bottom).offset(38)
-      $0.leading.trailing.equalToSuperview().inset(16)
-      $0.height.equalTo(130)
+
+    // MARK: Properties
+
+    var depaturePlace: Place?
+    var destinationPlace: Place?
+    var isDeparture: Bool = true
+    var singletonData: SingletonUserData = SingletonUserData.shared
+    let searchPathAPI = SearchPathAPI()
+
+
+    // MARK: View LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configuration()
+        self.layout()
     }
-    self.trandsferLeastButton.snp.makeConstraints{
-      $0.top.equalTo(self.timeLeastButton.snp.bottom).offset(38)
-      $0.leading.trailing.equalToSuperview().inset(16)
-      $0.height.equalTo(130)
+
+    private func configuration() {
+        self.configureNavigation()
     }
-  }
+
+    private func configureNavigation() {
+        self.navigationItem.largeTitleDisplayMode = .always
+        self.navigationItem.title = "Select Route"
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
+    }
+
+
+    // MARK: Layout
+
+    private func layout() {
+        navigationController?.navigationBar.backgroundColor = UIColor(named: "naviBlack")
+
+        self.RecommandButton.layer.cornerRadius = ViewMetrics.buttonCornerRadius
+        self.TimeLeastButton.layer.cornerRadius = ViewMetrics.buttonCornerRadius
+        self.TransferLeastButton.layer.cornerRadius = ViewMetrics.buttonCornerRadius
+    }
 }
