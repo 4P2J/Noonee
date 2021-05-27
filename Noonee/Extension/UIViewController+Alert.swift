72de86d415f0868929e6df4ff3d2e00e37042da1
조상호 <97chos@naver.com>
2021-05-27 15:04:57 +0900

    Alert Extension 구현

--- Noonee/Extension/UIViewController+Alert.swift
+++ Noonee/Extension/UIViewController+Alert.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    func alert(message: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+                self.navigationController?.popViewController(animated: true)
+            })
+            self.present(alert, animated: true)
+        }
+    }
+}
